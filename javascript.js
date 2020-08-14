e5a7d9d8c0a96e36acdadc79b32df83f8897df5f
Kévin-Nieto <K_nieto@hotmail.fr>
2020-08-14 18:39:29 +0200

    Mise à jour du code javascript

--- javascript.js
+++ javascript.js
@@ -3,74 +3,31 @@ fetch('http://localhost:3000/api/teddies') //On va sur l'adresse de l'API
   	return response.json(); //On récupère les données au format json
 })
 .then(function(data) {
-	const nounours1 = data[0];
-	let img1 = document.createElement("img"); //On crée un élément img et récupère sa source dans l'API
-	img1.src = "http://localhost:3000/images/teddy_1.jpg";
 	
-	
-	let div1 = document.getElementById("block1"); //Ici on va récupérer le nom, déscription et prix du 1er nounours et l'ajouter au HTML
-	div1.appendChild(img1);
-	let nom1 = document.querySelector(".nom1");
-	nom1.textContent = data[0].name;
-	let info1 = document.querySelector(".description1");
-	info1.textContent = data[0].description;
-	let prix1 = document.querySelector(".prix1");
-	prix1.textContent = data[0].price;
+for (i = 0; i < 5; i++) {
+		let nounours = data[i];
+		let img1 = document.createElement("img"); //On crée un élément img et récupère sa source dans l'API
+		img1.src = data[i].imageUrl;
+		let div1 = document.getElementById("block1"); //Ici on va récupérer le nom, déscription et prix du 1er nounours et l'ajouter au HTML
+		div1.appendChild(img1);
+		let nom1 = document.querySelector(".nom1");
+		nom1.textContent = data[i].name;
+		let info1 = document.querySelector(".description1");
+		info1.textContent = data[i].description;
+		let prix1 = document.querySelector(".prix1");
+		prix1.textContent = data[i].price;
+		console.log(nounours);
+	}
 
-	const nounours2 = data[1];
-	let img2 = document.createElement("img"); //On fait la même chose pour les nounours réstants
-	img2.src = "http://localhost:3000/images/teddy_2.jpg";
-	
-	let div2 = document.getElementById("block2");
-	div2.appendChild(img2);
-	let nom2 = document.querySelector(".nom2");
-	nom2.textContent = data[1].name;
-	let info2 = document.querySelector(".description2");
-	info2.textContent = data[1].description;
-	let prix2 = document.querySelector(".prix2");
-	prix2.textContent = data[1].price;
+	for (let valeur of data) {
+		let newDiv = document.createElement("div");
+		let nom = document.createTextNode(valeur.name);
+		
+		let currentDiv = document.getElementById("block1");
 
-	const nounours3 = data[2];
-	let img3 = document.createElement("img");
-	img3.src = "http://localhost:3000/images/teddy_3.jpg";
-	
-	let div3 = document.getElementById("block3");
-	div3.appendChild(img3);
-	let nom3 = document.querySelector(".nom3");
-	nom3.textContent = data[2].name;
-	let info3 = document.querySelector(".description3");
-	info3.textContent = data[2].description;
-	let prix3 = document.querySelector(".prix3");
-	prix3.textContent = data[2].price;
-
-	const nounours4 = data[3];
-	let img4 = document.createElement("img");
-	img4.src = "http://localhost:3000/images/teddy_4.jpg";
-	
-	let div4 = document.getElementById("block4");
-	div4.appendChild(img4);
-	let nom4 = document.querySelector(".nom4");
-	nom4.textContent = data[3].name;
-	let info4 = document.querySelector(".description4");
-	info4.textContent = data[3].description;
-	let prix4 = document.querySelector(".prix4");
-	prix4.textContent = data[3].price;
-
-	const nounours5 = data[4];
-	let img5 = document.createElement("img");
-	img5.src = "http://localhost:3000/images/teddy_5.jpg";
-	
-	let div5 = document.getElementById("block5");
-	div5.appendChild(img5);
-	let nom5 = document.querySelector(".nom5");
-	nom5.textContent = data[4].name;
-	let info5 = document.querySelector(".description5");
-	info5.textContent = data[4].description;
-	let prix5 = document.querySelector(".prix5");
-	prix5.textContent = data[4].price;
-
-	
-	
-
+		currentDiv.appendChild(nom);
+		
+		console.log(valeur);
+	}
 	
 })
--- javascript2.js
+++ javascript2.js
@@ -1,30 +1,87 @@
-fetch('http://localhost:3000/api/teddies/5be9c8541c9d440000665243') 
+fetch('http://localhost:3000/api/teddies') 
 .then(function(response) {
 	return response.json();
   	
 })
 .then(function(data) {
-	const nounours6 = data; //On fait la même chose que dans le fichier javascript.js, pour afficher les images et infos des nounours
+	const nounours6 = data[0]; //On fait la même chose que dans le fichier javascript.js, pour afficher les images et infos des nounours
 	let img6 = document.createElement("img");
-	img6.src = data.imageUrl;
+	img6.src = data[0].imageUrl;
 	
 	let div6 = document.getElementById("Produit");
 	div6.appendChild(img6);
 	let nom6 = document.querySelector(".nom0");
-	nom6.textContent = data.name;
+	nom6.textContent = data[0].name;
 	let info6 = document.querySelector(".description0");
-	info6.textContent = data.description;
+	info6.textContent = data[0].description;
 	let prix6 = document.querySelector(".prix0");
-	prix6.textContent = data.price;
-	let select = document.getElementById("selectNumber");
-	let options = [data.colors[0],data.colors[1],data.colors[2],data.colors[3]];
+	prix6.textContent = data[0].price;
+	
+	let img7 = document.createElement("img");
+	img7.src = "http://localhost:3000/images/teddy_2.jpg";
+	
+	let div7 = document.querySelector(".img1");
+	div7.appendChild(img7);
 
-	for(let i = 0; i < options.length; i++) {
-		let opt = options[i];
-		let el = document.createElement("option");
-		el.textContent = opt;
-		el.value = opt;
-		select.appendChild(el);
+	let img8 = document.createElement("img");
+	img8.src = "http://localhost:3000/images/teddy_3.jpg";
+	
+	let div8 = document.querySelector(".img2");
+	div8.appendChild(img8);
+
+	let img9 = document.createElement("img");
+	img9.src = "http://localhost:3000/images/teddy_4.jpg";
+	
+	let div9 = document.querySelector(".img3");
+	div9.appendChild(img9);
+
+	let img10 = document.createElement("img");
+	img10.src = "http://localhost:3000/images/teddy_5.jpg";
+	
+	let div10 = document.querySelector(".img4");
+	div10.appendChild(img10);
+
+	let id = window.location.search; //Chaque nounours possède un lien avec un id, en fonction de celui-ci, on change les images et infos de notre page
+	if (id === "?_id=5beaa8bf1c9d440000a57d94") //Si notre id correspond à celui de la barre d'adresse, alors on exécute ce code
+	{
+		img6.src = "http://localhost:3000/images/teddy_2.jpg"; //On change l'image principale de la page2 par celle-ci
+		nom6.textContent = data[1].name; //Modification des anciènes informations par les nouvelles
+		info6.textContent = data[1].description;
+		prix6.textContent = data[1].price;
+		img7.src = "http://localhost:3000/images/teddy_1.jpg"; //On remplace la miniature par l'anciène image principale
+		let newlien1 = document.getElementById('lien1');
+		newlien1.href = "page2.html?_id=5be9c8541c9d440000665243"; //On change le lien de la miniature pour arriver sur le nounours correspondant
+		
+	}
+	else if (id === "?_id=5beaaa8f1c9d440000a57d95") //On exécute le même code en changeant les valeurs pour chaque nounours
+	{
+		img6.src = "http://localhost:3000/images/teddy_3.jpg";
+		nom6.textContent = data[2].name;
+		info6.textContent = data[2].description;
+		prix6.textContent = data[2].price;
+		img8.src = "http://localhost:3000/images/teddy_1.jpg";
+		let newlien2 = document.getElementById('lien2');
+		newlien2.href = "page2.html?_id=5be9c8541c9d440000665243";
+	}
+	else if (id === "?_id=5beaabe91c9d440000a57d96")
+	{
+		img6.src = "http://localhost:3000/images/teddy_4.jpg";
+		nom6.textContent = data[3].name;
+		info6.textContent = data[3].description;
+		prix6.textContent = data[3].price;
+		img9.src = "http://localhost:3000/images/teddy_1.jpg";
+		let newlien3 = document.getElementById('lien3');
+		newlien3.href = "page2.html?_id=5be9c8541c9d440000665243";
+	}
+	else if (id === "?_id=5beaacd41c9d440000a57d97")
+	{
+		img6.src = "http://localhost:3000/images/teddy_5.jpg";
+		nom6.textContent = data[4].name;
+		info6.textContent = data[4].description;
+		prix6.textContent = data[4].price;
+		img10.src = "http://localhost:3000/images/teddy_1.jpg";
+		let newlien4 = document.getElementById('lien4');
+		newlien4.href = "page2.html?_id=5be9c8541c9d440000665243";
 	}
 
 	
